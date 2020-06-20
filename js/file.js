b6caa16560b63657a43c4d5231a13cce1d15312f
Sunil Adari <asunilsingh2205@gmail.com>
2020-06-20 17:18:18 +0530

    added some animation to buttons & active navLink on scrolling

--- js/file.js
+++ js/file.js
@@ -13,8 +13,13 @@ const navSlide = () => {
 	const nav = document.querySelector(".items");
 	const navLinks = document.querySelectorAll(".items li");
 
+	const burgerIcon = document.querySelector(".burger");
+
 	burger.addEventListener("click", () => {
 		nav.classList.toggle("nav-active");
+		burgerIcon.children[0].classList.toggle("line1");
+		burgerIcon.children[1].classList.toggle("line2");
+		burgerIcon.children[2].classList.toggle("line3");
 
 		navLinks.forEach((link, index, event) => {
 			if (link.style.animation) {
@@ -27,5 +32,33 @@ const navSlide = () => {
 		});
 	});
 };
+
+// scroll active navlink logic
+function onScroll(event) {
+	var sections = document.querySelectorAll("#navbar ul li a");
+	var scrollPos =
+		window.pageYOffset ||
+		document.documentElement.scrollTop ||
+		document.body.scrollTop;
+
+	for (var i = 0; i < sections.length; i++) {
+		var currLink = sections[i];
+		var val = currLink.getAttribute("href");
+		var refElement = document.querySelector(val);
+		if (
+			refElement.offsetTop <= scrollPos &&
+			refElement.offsetTop + refElement.offsetHeight > scrollPos
+		) {
+			document
+				.querySelector("#navbar ul li a")
+				.classList.remove("active");
+			currLink.classList.add("active");
+		} else {
+			currLink.classList.remove("active");
+		}
+	}
+}
+
+window.document.addEventListener("scroll", onScroll);
 
 navSlide();
